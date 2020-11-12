c67b5282223191fef61f237d1791a8ae61b4b8e0
Corey Miller <CoreyMiller454@gmail.com>
2020-11-11 21:19:45 -0500

    add trash can

--- client/src/Component/Task.js
+++ client/src/Component/Task.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import {useHistory} from 'react-router'
+import React from "react";
+import { useHistory } from "react-router";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
 
 const Task = (props) => {
   const history = useHistory();
 
- const handleClick = (id) => {
-    history.push("/api/task/" + id)
-  }
+  const handleClick = (id) => {
+    history.push("/api/task/" + id);
+  };
 
+  const handleDelete = () => {
+    const response = axios.delete(
+      "http://localhost:5000/api/task/" + props.data._id
+    );
+  };
 
   return (
-    <div className="task-div" onClick={() => handleClick(props.data._id)} >
+    <div className="task-div" onClick={() => handleClick(props.data._id)}>
       <h1 className="task-title">{props.data.title}</h1>
-      <p>{props.data.materials}</p> 
-      <p>{props.data.importance}</p> 
-
-
+      <p>{props.data.materials}</p>
+      <p>{props.data.importance}</p>
+      <FontAwesomeIcon icon={faTrash} onClick={handleDelete} />
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
--- client/src/Component/TaskExpanded.js
+++ client/src/Component/TaskExpanded.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 import { config } from "../constants";
 
 class TaskExpanded extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      task: ''
-    }
+      task: "",
+    };
   }
 
   componentDidMount = async () => {
-    const response = await axios.get(config.url.API_URL + "/api/task/" + this.props.match.params.id)
+    const response = await axios.get(
+      config.url.API_URL + "/api/task/" + this.props.match.params.id
+    );
     this.setState({
-      task: response.data.data
-    })
-  }
+      task: response.data.data,
+    });
+  };
   render() {
-
     return (
       <div>
-      <h2>Task Title: {this.state.task.title}</h2>
-    </div>
-  )
-}
+        <h2>Task Title: {this.state.task.title}</h2>
+        {/* <h3>Task Materials: {this.state.task.materials}</h2>
+        <h3>Task Imporantance: {this.state.task.importance}</h3> */}
+      </div>
+    );
+  }
 }
 
-export default TaskExpanded
+export default TaskExpanded;
