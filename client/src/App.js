8be61565defc1da817f05682ac6205ab307925fa
Corey Miller <coreymiller454@gmail.com>
2020-12-15 18:25:53 -0500

    testing for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,15 @@ import Header from "./Component/Header";
 function App() {
   return (
     <div>
-      <Header />
+      Hello from Project Planner
+      {/* <Header />
       <Route exact path="/">
         <List />
       </Route>
       <Switch>
         <Route path="/newtask" component={NewTask} />
         <Route path="/api/task/:id" component={TaskExpanded} />
-      </Switch>
+      </Switch> */}
     </div>
   );
 }
